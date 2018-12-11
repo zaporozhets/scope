974370d0a78de34661f706dba922ef6194c20a32
Taras Zaporozhets <zaporozhets.taras@gmail.com>
2018-12-11 23:07:33 +0200

    Add script for update over ssh

---
+++ ssh_update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+
+THIS_SCRIPT_DIR=$(readlink -f $(dirname $0))
+
+TOP_DIR=$(readlink -f "${THIS_SCRIPT_DIR}/")
+
+
+################################################################################
+# Main function
+################################################################################
+function MAIN
+{
+
+    TARGET="dev"
+
+    echo "Using target: ${TARGET}"
+
+    scp ${TOP_DIR}/bsp/buildroot/output/images/scope_latest.swu ${TARGET}:/mnt/userdata/
+
+    ssh ${TARGET} "swupdate -v -i /mnt/userdata/scope_latest.swu"
+
+    ssh ${TARGET} "reboot"
+}
+
+################################################################################
+#
+################################################################################
+function TRAP_CTRL_C() {
+    LOG_ERROR "** Trapped CTRL-C"
+    exit -1
+}
+
+MAIN $@
+
+exit $?
